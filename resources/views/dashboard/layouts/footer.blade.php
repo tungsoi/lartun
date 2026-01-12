6a2c5596bfb59a2bb6bff5b22daee6a0ca795d0a
tung.dt8 <tung.dt8@shbfinance.com.vn>
2026-01-12 20:55:40 +0700

    update

---
+++ resources/views/dashboard/layouts/footer.blade.php
@@ -0,0 +1,107 @@
+<script>
+    if (document.documentElement.className.search("hs-inline-edit") !== -1) {
+        document.write('<script src=".//2689945.fs1.hubspotusercontent-na1.net/hubfs/2689945/hub_generated/template_assets/1/63656656786/1767632358555/template_admin.min.js"><\x2fscript>');
+        document.write('<link rel="stylesheet" href="//2689945.fs1.hubspotusercontent-na1.net/hubfs/2689945/hub_generated/template_assets/1/63656557187/1767632356195/template_admin.min.css">');
+    }
+</script>
+<!-- HubSpot performance collection script -->
+<script defer src="./hs/hsstatic/content-cwv-embed/static-1.1293/embed.js"></script>
+<script>
+    var hsVars = hsVars || {};
+    hsVars['language'] = 'en';
+</script>
+<script src="./hs/hsstatic/cos-i18n/static-1.53/bundles/project.js"></script>
+<!-- Start of Web Interactives Embed Code -->
+<script defer src="https://js.hubspot.com/web-interactives-embed.js" type="text/javascript"
+        id="hubspot-web-interactives-loader" data-loader="hs-previewer" data-hsjs-portal="2689945" data-hsjs-env="prod"
+        data-hsjs-hublet="na1"></script>
+<!-- End of Web Interactives Embed Code -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/3.4.0/jquery-migrate.min.js"></script>
+<script src="./hubfs/hub_generated/module_assets/1/182385787549/1744340403534/module_Hero_Section.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+<script>
+    $('.hero_section__row').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 8000,
+        speed: 600,
+        dots: true,
+        arrows: false,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        //      adaptiveHeight: true
+
+
+    });
+</script>
+<script src="./hubfs/hub_generated/module_assets/1/199295892120/1762793283535/module_stats.min.js"></script>
+<script src="./hubfs/hub_generated/module_assets/1/153482232588/1742504555697/module_tabs.min.js"></script>
+<script src="./hubfs/hub_generated/module_assets/1/64556426251/1744340380709/module_tab-slider.min.js"></script>
+<!-- Start of HubSpot Analytics Code -->
+<script type="text/javascript">
+    var _hsq = _hsq || [];
+    _hsq.push(["setContentType", "standard-page"]);
+    _hsq.push(["setCanonicalUrl", "https:\/\/www.cobalt.io"]);
+    _hsq.push(["setPageId", "174329775675"]);
+    _hsq.push(["setContentMetadata", {
+        "contentPageId": 174329775675,
+        "legacyPageId": "174329775675",
+        "contentFolderId": null,
+        "contentGroupId": null,
+        "abTestId": 174329775673,
+        "languageVariantId": 174329775675,
+        "languageCode": "en",
+
+
+    }]);
+</script>
+<script type="text/javascript" id="hs-script-loader" async defer src="./hs/scriptloader/2689945.js"></script>
+<!-- End of HubSpot Analytics Code -->
+<script type="text/javascript">
+    var hsVars = {
+        render_id: "a996fec7-733d-43db-96b2-45b1bfbbe2d2",
+        ticks: 1767652098542,
+        page_id: 174329775675,
+
+        content_group_id: 0,
+        portal_id: 2689945,
+        app_hs_base_url: "https://app.hubspot.com",
+        cp_hs_base_url: "https://cp.hubspot.com",
+        language: "en",
+        analytics_page_type: "standard-page",
+        scp_content_type: "",
+
+        analytics_page_id: "174329775675",
+        category_id: 1,
+        folder_id: 0,
+        is_hubspot_user: false
+    }
+</script>
+<script defer src="{{ }}"></script>
+<link rel="preload" href="{{ asset('dashboard/hubfs/hub_generated/template_assets/1/63656476501/1767632363311/template_bulma-grid.min.css') }}"
+      as="style" onload="this.onload=null;this.rel='stylesheet'">
+<noscript>
+    <link rel="stylesheet"
+          href="{{ asset('dashboard/hubfs/hub_generated/template_assets/1/63656476501/1767632363311/template_bulma-grid.min.css') }}">
+</noscript>
+<link rel="preload" href="{{ asset('dashboard/hubfs/hub_generated/template_assets/1/63656838307/1767632355590/template_flickity.min.css') }}"
+      as="style" onload="this.onload=null;this.rel='stylesheet'">
+<noscript>
+    <link rel="stylesheet"
+          href="{{ asset('dashboard/hubfs/hub_generated/template_assets/1/63656838307/1767632355590/template_flickity.min.css') }}">
+</noscript>
+<link defer async rel="preload"
+      href="{{ asset('dashboard/hubfs/hub_generated/template_assets/1/69949528887/1767632360412/template_body.min.css') }}" as="style"
+      onload="this.onload=null;this.rel='stylesheet'">
+<noscript>
+    <link rel="stylesheet" href="{{ asset('dashboard/hubfs/hub_generated/template_assets/1/69949528887/1767632360412/template_body.min.css') }}">
+</noscript>
+<script src="{{ asset('dashboard/hubfs/hub_generated/template_assets/1/63656577959/1767632366931/template_jquery.magnificpopup.min.js') }}"></script>
+<script src="{{ asset('dashboard/hubfs/hub_generated/template_assets/1/63656711346/1767632360434/template_waypoints.min.js') }}"></script>
+<script src="{{ asset('dashboard/hubfs/hub_generated/template_assets/1/63656476504/1767632367818/template_waypoints.inview.min.js') }}"></script>
+<script src="{{ asset('dashboard/hubfs/hub_generated/template_assets/1/63656557191/1767632360120/template_flickity-combo.min.js') }}"></script>
+<script src="{{ asset('dashboard/hubfs/hub_generated/template_assets/1/63656750153/1767632361544/template_frontend.min.js') }}"></script>
+<script src="{{ asset('dashboard/hubfs/hub_generated/template_assets/1/61650962256/1767632364795/template_main.min.js') }}"></script>
